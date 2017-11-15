cbc7f70b4d6048349e1c7cb560075ce7ebd30c55
mistval <randallschmidt1@gmail.com>
2017-11-15 11:27:11 -0600

    catch errors while reloading

--- core/commands/reload.js
+++ core/commands/reload.js
@@ -1,6 +1,7 @@
 'use strict'
 const reload = require('require-reload')(require);
 const ErisUtils = reload('./../util/eris_utils.js');
+const logger = require('./../logger.js');
 
 /**
 * A command for reloading the command and message managers. This is a special command that the command manager has direct knowledge of.
@@ -18,8 +19,14 @@ class Reload {
   }
 
   execute_(bot, msg, suffix, reloadAction) {
-    reloadAction();
-    ErisUtils.sendMessageAndDelete(msg, 'Reloaded!');
+    try {
+      reloadAction();
+      ErisUtils.sendMessageAndDelete(msg, 'Reloaded!');
+    } catch (err) {
+      let errorMessage = 'There was an unhandled error while reloading. Monochrome will continue to run, but may be in a bad state. You should restart it as soon as possible. Check the logs for more details.';
+      msg.channel.createMessage(errorMessage);
+      logger.logFailure('RELOAD', errorMessage, err);
+    }
   }
 }
 
