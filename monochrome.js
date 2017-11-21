a05e9d3b4994c1c797cf013b8b83ef1eddaaa18b
mistval <randallschmidt1@gmail.com>
2017-11-20 20:21:08 -0600

    allow settings file to not exist

--- monochrome.js
+++ monochrome.js
@@ -1,9 +1,15 @@
 'use strict'
 const monochrome = require('./core/monochrome.js');
+const fs = require('fs');
 
 let configFilePath = __dirname + '/config.json';
 let commandsDirectoryPath = __dirname + '/commands';
 let messageProcessorsDirectoryPath = __dirname + '/message_processors';
 let settingsFilePath = __dirname + '/server_settings.json';
+
+if (!fs.existsSync(settingsFilePath)) {
+  settingsFilePath = undefined;
+}
+
 let bot = new monochrome(configFilePath, commandsDirectoryPath, messageProcessorsDirectoryPath, settingsFilePath);
 bot.connect();
