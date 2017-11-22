848664d7a319853cf272096aee5aa89f7f85b026
mistval <randallschmidt1@gmail.com>
2017-11-22 15:05:20 -0600

    add navigation stuff to exports

--- monochrome.js
+++ monochrome.js
@@ -4,3 +4,7 @@ module.exports.Bot = require('./core/bot.js');
 module.exports.PublicError = reload('./core/public_error.js');
 module.exports.logger = require('./core/logger.js');
 module.exports.persistence = require('./core/persistence.js');
+module.exports.NavigationPage = reload('./core/navigation_page.js');
+module.exports.NavigationChapter = reload('./core/navigation_chapter.js');
+module.exports.navigationManager = reload('./core/navigation_manager.js');
+module.exports.Navigation = reload('./core/navigation.js');
