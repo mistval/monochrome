74fca63c096f34a71c39a191f62b577ba87449a9
mistval <randallschmidt1@gmail.com>
2017-11-22 14:32:07 -0600

    create npm package

--- monochrome.js
+++ monochrome.js
@@ -1,15 +1,6 @@
 'use strict'
-const monochrome = require('./core/monochrome.js');
-const fs = require('fs');
-
-let configFilePath = __dirname + '/config.json';
-let commandsDirectoryPath = __dirname + '/commands';
-let messageProcessorsDirectoryPath = __dirname + '/message_processors';
-let settingsFilePath = __dirname + '/server_settings.json';
-
-if (!fs.existsSync(settingsFilePath)) {
-  settingsFilePath = undefined;
-}
-
-let bot = new monochrome(configFilePath, commandsDirectoryPath, messageProcessorsDirectoryPath, settingsFilePath);
-bot.connect();
+const reload = require('require-reload')(require);
+module.exports.Bot = require('./core/bot.js');
+module.exports.PublicError = reload('./core/public_error.js');
+module.exports.logger = require('./core/logger.js');
+module.exports.persistence = require('./core/persistence.js');
