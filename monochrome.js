ec6be93f06eb1f917e39b41afe3cb8fd1906b25f
mistval <randallschmidt1@gmail.com>
2017-11-28 03:18:14 -0600

    navigationManager, as a singleton, should be required not reloaded. or else bad things can happen

--- monochrome.js
+++ monochrome.js
@@ -6,5 +6,5 @@ module.exports.logger = require('./core/logger.js');
 module.exports.persistence = require('./core/persistence.js');
 module.exports.NavigationPage = reload('./core/navigation_page.js');
 module.exports.NavigationChapter = reload('./core/navigation_chapter.js');
-module.exports.navigationManager = reload('./core/navigation_manager.js');
+module.exports.navigationManager = require('./core/navigation_manager.js');
 module.exports.Navigation = reload('./core/navigation.js');
