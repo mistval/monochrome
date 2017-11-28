6b5c30483741bcb72a38b6a14ec1b6d3485827e6
mistval <randallschmidt1@gmail.com>
2017-11-28 02:23:16 -0600

    export the bot constructor as the root export (didnt realize you could do that before

--- monochrome.js
+++ monochrome.js
@@ -1,6 +1,6 @@
 'use strict'
 const reload = require('require-reload')(require);
-module.exports.Bot = require('./core/bot.js');
+module.exports = require('./core/bot.js');
 module.exports.PublicError = reload('./core/public_error.js');
 module.exports.logger = require('./core/logger.js');
 module.exports.persistence = require('./core/persistence.js');
