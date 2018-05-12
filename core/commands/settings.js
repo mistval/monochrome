5976e5570888b9c94d6aac7ec50d5b5a627fb60e
Randall Schmidt <randallschmidt1@gmail.com>
2018-05-12 13:44:47 -0500

    get setting settings working (only lightly tested and could probably be cleaned up a bit

--- core/commands/settings.js
+++ core/commands/settings.js
@@ -1,4 +1,6 @@
-const Hook = require('./../message_processors/user_and_channel_hook.js');
+const reload = require('require-reload')(require);
+const assert = require('assert');
+const Hook = reload('./../message_processors/user_and_channel_hook.js');
 
 const CATEGORY_DESCRIPTION = 'The following subcategories and settings are available. Type the number of the one you want to see/change.';
 const HOOK_EXPIRATION_MS = 180000;
@@ -124,12 +126,12 @@ function findParent(children, targetNode, previous) {
   return undefined;
 }
 
-function tryGoBack(hook, msg, monochrome, settingsNode, color, root) {
+function tryGoBack(hook, msg, monochrome, settingsNode, color, root, userIsServerAdmin) {
   if (msg.content.toLowerCase() === 'back') {
     const parent = findParent(monochrome.getSettings().getRawSettingsTree(), settingsNode, root);
     if (parent) {
       hook.unregister();
-      return showNode(monochrome, msg, color, parent);
+      return showNode(monochrome, msg, color, parent, userIsServerAdmin);
     }
   }
 
@@ -145,34 +147,164 @@ function tryCancel(hook, msg) {
   return false;
 }
 
-function handleRootViewMsg(hook, monochrome, msg, color) {
+function handleRootViewMsg(hook, monochrome, msg, color, userIsServerAdmin) {
   const index = messageToIndex(msg);
   const settingsNodes = monochrome.getSettings().getRawSettingsTree();
   if (index < settingsNodes.length) {
     const nextNode = settingsNodes[index];
     hook.unregister();
-    return showNode(monochrome, msg, color, nextNode);
+    return showNode(monochrome, msg, color, nextNode, userIsServerAdmin);
   }
 
   return tryCancel(hook, msg);
 }
 
-function handleSettingViewMsg(hook, monochrome, msg, color, setting) {
+async function tryApplyNewSetting(hook, monochrome, msg, color, userIsServerAdmin, setting, newUserFacingValue, locationString) {
+  const settings = monochrome.getSettings();
+
   const cancelResult = tryCancel(hook, msg);
   if (cancelResult) {
     return cancelResult;
   }
 
-  return tryGoBack(hook, msg, monochrome, setting, color, monochrome.getSettings().getRawSettingsTree());
+  const backResult = tryGoBack(
+    hook,
+    msg,
+    monochrome,
+    setting,
+    color,
+    monochrome.getSettings().getRawSettingsTree(),
+    userIsServerAdmin,
+  );
+
+  if (backResult) {
+    return backResult;
+  }
+
+  const serverId = msg.channel.guild ? msg.channel.guild.id : msg.channel.id;
+  const locationStringLowerCase = locationString.toLowerCase();
+  let setResults;
+  let resultString;
+
+  if (locationStringLowerCase === 'me') {
+    resultString = 'The new setting has been applied as a user setting. It will take effect whenever you use the command.';
+    if (setting.serverOnly) {
+      return msg.channel.createMessage('That setting cannot be set as a user setting. Please say **this channel**, **server**, **cancel**, **back**, or provide a list of channels.');
+    }
+    setResults = [await settings.setUserSettingValue(setting.uniqueId, msg.author.id, newUserFacingValue)];
+  } else if (locationStringLowerCase === 'this channel' || !msg.channel.guild) {
+    resultString = 'The new setting has been applied to this channel.';
+    setResults = [await settings.setChannelSettingValue(setting.uniqueId, serverId, msg.channel.id, newUserFacingValue, userIsServerAdmin)];
+  } else if (locationStringLowerCase === 'server') {
+    resultString = 'The new setting has been applied to all channels in the server.';
+    setResults = [await settings.setServerWideSettingValue(setting.uniqueId, serverId, newUserFacingValue, userIsServerAdmin)];
+  } else {
+    resultString = `The new setting has been applied to the channels: ${locationString}`;
+    const regex = /<#(.*?)>/g;
+    const channelIds = [];
+
+    let regexResult;
+    while (regexResult = regex.exec(locationString)) {
+      const channelId = regexResult[1];
+      if (!msg.channel.guild.channels.find(channel => channel.id === channelId)) {
+        return msg.channel.createMessage(`I didn\'t find a channel in this server called **${regexResult[0]}**. Please check that the channel exists and try again.`);
+      }
+      channelIds.push(channelId);
+    }
+
+    const promises = [];
+    for (const channelId of channelIds) {
+      promises.push(settings.setChannelSettingValue(setting.uniqueId, serverId, channelId, newUserFacingValue, userIsServerAdmin));
+    }
+
+    setResults = await Promise.all(promises);
+  }
+
+  hook.unregister();
+
+  for (const result of setResults) {
+    if (!result.accepted) {
+      monochrome.getLogger().logFailure('SETTINGS', `Unexpected setting update rejection. Reason: ${result.reason}`);
+      return msg.channel.createMessage('There was an error updating that setting. Sorry. I\'ll look into it!');
+    }
+  }
+
+  return msg.channel.createMessage(resultString);
 }
 
-function handleCategoryViewMsg(hook, monochrome, msg, color, category) {
+function promptForSettingLocation(hook, msg, monochrome, settingNode, color, userIsServerAdmin, newUserFacingValue) {
+  hook.unregister();
+
+  hook = Hook.registerHook(
+    msg.author.id,
+    msg.channel.id,
+    (cbHook, cbMsg, monochrome) => tryApplyNewSetting(
+      cbHook,
+      monochrome,
+      cbMsg,
+      color,
+      userIsServerAdmin,
+      settingNode,
+      newUserFacingValue,
+      cbMsg.content,
+    ),
+    monochrome.getLogger(),
+  );
+
+  hook.setExpirationInMs(HOOK_EXPIRATION_MS, () => handleExpiration(msg));
+  if (settingNode.serverOnly) {
+    return msg.channel.createMessage('Where should the new setting be applied? You can say **server**, **this channel**, or list channels, for example: **#general #bot #quiz**. You can also say **cancel** or **back**.');
+  } else {
+    return msg.channel.createMessage('Where should the new setting be applied? You can say **me**, **server**, **this channel**, or list channels, for example: **#general #bot #quiz**. You can also say **cancel** or **back**.');
+  }
+}
+
+async function handleSettingViewMsg(hook, monochrome, msg, color, setting, userIsServerAdmin) {
+  assert(typeof userIsServerAdmin === 'boolean', 'userIsServerAdmin is not boolean');
+
+  const cancelResult = tryCancel(hook, msg);
+  if (cancelResult) {
+    return cancelResult;
+  }
+
+  const tryGoBackResult = tryGoBack(
+    hook,
+    msg,
+    monochrome,
+    setting,
+    color,
+    monochrome.getSettings().getRawSettingsTree(),
+    userIsServerAdmin,
+  );
+
+  if (tryGoBackResult) {
+    return tryGoBackResult;
+  }
+
+  const settings = monochrome.getSettings();
+  const newUserFacingValue = msg.content;
+
+  const isValid = await settings.userFacingValueIsValidForSetting(setting, newUserFacingValue);
+  if (!isValid) {
+    return msg.channel.createMessage('That isn\'t a valid value for that setting. Please check the **Allowed values** and try again. You can also say **back** or **cancel**.');
+  }
+
+  if (userIsServerAdmin) {
+    return promptForSettingLocation(hook, msg, monochrome, setting, color, userIsServerAdmin, newUserFacingValue);
+  } else if (setting.serverOnly) {
+    return msg.channel.createMessage('Only a server admin can set that setting. You can say **back** or **cancel**.');
+  } else {
+    return tryApplyNewSetting(hook, monochrome, msg, color, userIsServerAdmin, setting, newUserFacingValue, 'me');
+  }
+}
+
+function handleCategoryViewMsg(hook, monochrome, msg, color, category, userIsServerAdmin) {
   const index = messageToIndex(msg);
   const childNodes = category.children;
   if (index < childNodes.length) {
     const nextNode = childNodes[index];
     hook.unregister();
-    return showNode(monochrome, msg, color, nextNode);
+    return showNode(monochrome, msg, color, nextNode, userIsServerAdmin);
   }
 
   const cancelResult = tryCancel(hook, msg);
@@ -180,10 +312,18 @@ function handleCategoryViewMsg(hook, monochrome, msg, color, category) {
     return cancelResult;
   }
 
-  return tryGoBack(hook, msg, monochrome, category, color, monochrome.getSettings().getRawSettingsTree());
+  return tryGoBack(
+    hook,
+    msg,
+    monochrome,
+    category,
+    color,
+    monochrome.getSettings().getRawSettingsTree(),
+    userIsServerAdmin,
+  );
 }
 
-function showRoot(monochrome, msg, color) {
+function showRoot(monochrome, msg, color, userIsServerAdmin) {
   const rootContent = createContentForRoot(monochrome.getSettings().getRawSettingsTree(), color);
   const hook = Hook.registerHook(
     msg.author.id,
@@ -193,6 +333,7 @@ function showRoot(monochrome, msg, color) {
       monochrome,
       cbMsg,
       color,
+      userIsServerAdmin,
     ),
     monochrome.getLogger(),
   );
@@ -201,7 +342,7 @@ function showRoot(monochrome, msg, color) {
   return msg.channel.createMessage(rootContent);
 }
 
-function showCategory(monochrome, msg, color, category) {
+function showCategory(monochrome, msg, color, category, userIsServerAdmin) {
   const categoryContent = createContentForCategory(category, color);
   const hook = Hook.registerHook(
     msg.author.id, msg.channel.id,
@@ -211,6 +352,7 @@ function showCategory(monochrome, msg, color, category) {
       cbMsg,
       color,
       category,
+      userIsServerAdmin,
     ),
     monochrome.getLogger(),
   );
@@ -219,7 +361,7 @@ function showCategory(monochrome, msg, color, category) {
   return msg.channel.createMessage(categoryContent);
 }
 
-async function showSetting(monochrome, msg, color, setting) {
+async function showSetting(monochrome, msg, color, setting, userIsServerAdmin) {
   const settingContent = await createContentForSetting(msg, monochrome.getSettings(), setting, color);
   const hook = Hook.registerHook(
     msg.author.id, msg.channel.id,
@@ -229,6 +371,7 @@ async function showSetting(monochrome, msg, color, setting) {
       cbMsg,
       color,
       setting,
+      userIsServerAdmin,
     ),
     monochrome.getLogger(),
   );
@@ -237,18 +380,18 @@ async function showSetting(monochrome, msg, color, setting) {
   return msg.channel.createMessage(settingContent);
 }
 
-function showNode(monochrome, msg, color, node) {
+function showNode(monochrome, msg, color, node, userIsServerAdmin) {
   if (Array.isArray(node)) {
-    return showRoot(monochrome, msg, color);
+    return showRoot(monochrome, msg, color, userIsServerAdmin);
   } else if (node.children) {
-    return showCategory(monochrome, msg, color, node);
+    return showCategory(monochrome, msg, color, node, userIsServerAdmin);
   } else {
-    return showSetting(monochrome, msg, color, node);
+    return showSetting(monochrome, msg, color, node, userIsServerAdmin);
   }
 }
 
 function execute(monochrome, msg, suffix, color) {
-  return showNode(monochrome, msg, color, monochrome.getSettings().getRawSettingsTree());
+  return showNode(monochrome, msg, color, monochrome.getSettings().getRawSettingsTree(), msg.authorIsServerAdmin);
 }
 
 class SettingsCommand {
@@ -256,6 +399,7 @@ class SettingsCommand {
     this.commandAliases = config.settingsCommandAliases;
     this.uniqueId = 'autoGeneratedSettings425654';
     this.canBeChannelRestricted = false;
+    this.attachIsServerAdmin = true;
 
     this.action = (erisBot, monochrome, msg, suffix) => execute(
       monochrome,
