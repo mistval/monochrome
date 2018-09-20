04f6bd86bf806870778b55da034d11c2e38c0eb1
mistval <randallschmidt1@gmail.com>
2018-09-20 18:06:05 +0800

    add jsdoc comments for persistence

--- core/persistence.js
+++ core/persistence.js
@@ -12,6 +12,16 @@ function keyForServerId(serverId) {
   return SERVER_DATA_KEY_PREFIX + serverId;
 }
 
+/**
+ * @callback Persistence~editFunction
+ * @param {Object} data - The current data associated with the key.
+ * @returns {Object} The new data to associate with the key.
+ */
+
+/**
+ * Read or write persistent data that is persisted even if the process is killed.
+ * @hideconstructor
+ */
 class Persistence {
   constructor(defaultPrefixes, logger, nodePersistOptions) {
     storage.init(nodePersistOptions);
@@ -25,42 +35,85 @@ class Persistence {
     });
   }
 
-  getData(key) {
-    return storage.getItem(key).then(data => {
-      if (data) {
-        return data;
-      } else {
-        return {};
-      }
-    });
+  /**
+   * Get the value associated with the specified key. If no such value exists,
+   * an empty object {} is returned.
+   * @param {string} key
+   * @returns {Object} The value associated with the specified key.
+   */
+  async getData(key) {
+    const data = await storage.getItem(key);
+    if (data) {
+      return data;
+    } else {
+      return {};
+    }
   }
 
-  getDataForUser(userId) {
+  /**
+   * Get the data associated with a user. If no such data exists, an empty
+   * object {} is returned.
+   * @param {string} userId
+   * @returns {Object} The value associated with the specified userId
+   */
+  async getDataForUser(userId) {
     return this.getData(keyForUserId(userId));
   }
 
-  getDataForServer(serverId) {
+  /**
+   * Get the data associated with a server. If no such data exists, an empty
+   * object {} is returned.
+   * @param {string} serverId
+   * @returns {Object} The value associated with the specified serverId
+   */
+  async getDataForServer(serverId) {
     return this.getData(keyForServerId(serverId));
   }
 
-  getGlobalData() {
+  /**
+   * Get the global data. If no such data exists, an empty
+   * object {} is returned.
+   * @returns {Object} The global data.
+   */
+  async getGlobalData() {
     return this.getData(GLOBAL_DATA_KEY);
   }
 
+  /**
+   * Get the command prefixes associated with a server.
+   * @param {string} serverId
+   * @returns {string[]}
+   */
   getPrefixesForServerId(serverId) {
     return this.prefixesForServerId_[serverId] || this.defaultPrefixes_;
   }
 
+  /**
+   * Get the primary prefix for the location where the specified msg was sent.
+   * @param {Eris.Message} msg
+   * @returns {string}
+   */
   getPrimaryPrefixFromMsg(msg) {
     const locationId = msg.channel.guild ? msg.channel.guild.id : msg.channel.id;
     return this.getPrefixesForServerId(locationId)[0];
   }
 
+  /**
+   * Get the prefixes for the location where the specified msg was sent.
+   * @param {Eris.Message} msg
+   * @returns {string[]}
+   */
   getPrefixesForMessage(msg) {
     return this.getPrefixesForServerId(msg.channel.guild ? msg.channel.guild.id : msg.channel.id);
   }
 
-  editData(key, editFunction) {
+  /**
+   * Edit the data associated with a key. This function is atomic in the sense
+   * that no one else can be editing the value for the same key at the same time.
+   * @param {string} key
+   * @param {Persistence~editFunction} editFunction - The function that performs the edit.
+   */
+  async editData(key, editFunction) {
     return storage.editItem(key, data => {
       if (!data) {
         data = {};
@@ -76,21 +129,43 @@ class Persistence {
     });
   }
 
-  editDataForUser(userId, editDataFunction) {
+  /**
+   * Edit the data associated with a user. This function is atomic in the sense
+   * that no one else can be editing the value for the same key at the same time.
+   * @param {string} userId
+   * @param {Persistence~editFunction} editFunction - The function that performs the edit.
+   */
+  async editDataForUser(userId, editFunction) {
     let key = keyForUserId(userId);
-    return this.editData(key, editDataFunction);
+    return this.editData(key, editFunction);
   }
 
-  editDataForServer(serverId, editDataFunction) {
+  /**
+   * Edit the data associated with a server. This function is atomic in the sense
+   * that no one else can be editing the value for the same key at the same time.
+   * @param {string} serverId
+   * @param {Persistence~editFunction} editFunction - The function that performs the edit.
+   */
+  async editDataForServer(serverId, editFunction) {
     let key = keyForServerId(serverId);
-    return this.editData(key, editDataFunction);
+    return this.editData(key, editFunction);
   }
 
-  editGlobalData(editDataFunction) {
-    return this.editData(GLOBAL_DATA_KEY, editDataFunction);
+  /**
+   * Edit the data global data. This function is atomic in the sense
+   * that no one else can be editing the value for the same key at the same time.
+   * @param {Persistence~editFunction} editFunction - The function that performs the edit.
+   */
+  async editGlobalData(editunction) {
+    return this.editData(GLOBAL_DATA_KEY, editFunction);
   }
 
-  editPrefixesForServerId(serverId, prefixes) {
+  /**
+   * Edit the prefixes associated with a server.
+   * @param {string} serverId
+   * @param {string[]} prefixes - The new prefixes for the server.
+   */
+  async editPrefixesForServerId(serverId, prefixes) {
     this.prefixesForServerId_[serverId] = prefixes;
     return this.editData(GLOBAL_DATA_KEY, data => {
       data.prefixes = data.prefixes || {};
@@ -99,13 +174,13 @@ class Persistence {
     });
   }
 
-  resetPrefixesForServerId(serverId) {
+  /**
+   * Reset the prefixes associated with a server.
+   * @param {string} serverId
+   */
+  async resetPrefixesForServerId(serverId) {
     delete this.prefixesForServerId_[serverId];
-    return this.editData(GLOBAL_DATA_KEY, data => {
-      data.prefixes = data.prefixes || {};
-      delete data.prefixes[serverId];
-      return data;
-    });
+    return this.editPrefixesForServerId(serverId, this.prefixesForServerId_);
   }
 }
 
