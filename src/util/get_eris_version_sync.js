03743d86fde240ad2cf0d8f893738b0daaddcada
mistval <randallschmidt1@gmail.com>
2023-04-22 12:51:12 -0400

    update docs

--- src/util/get_eris_version_sync.js
+++ src/util/get_eris_version_sync.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 function getErisVersionSync() {
   const indexPath = require.resolve('eris');
   const packagePath = path.join(path.dirname(indexPath), 'package.json');
-  const package = JSON.parse(fs.readFileSync(packagePath));
+  const packageJson = JSON.parse(fs.readFileSync(packagePath));
 
-  return package.version;
+  return packageJson.version;
 }
 
 module.exports = getErisVersionSync
